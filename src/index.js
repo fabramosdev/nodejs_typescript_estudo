996ccdd22e32ca5ba46b0cdd851dc4e8e1592018
Fabiano Santos Ramos <fabianosantosramos1978@gmail.com>
2025-03-27 08:44:35 -0300

    add http methods

--- src/index.js
+++ src/index.js
@@ -4,10 +4,29 @@ const app = express()
 
 const PORT = process.env.PORT
 
+const projects = [
+  "Project 1",
+  "Project 2"
+]
+
 app.get('/', (request, response) => {
-  return response.json({
-    message: "Olá Fabiano"
-  })
+  return response.json({ message: "Welcome to the API" })
+})
+
+app.get('/projects', (request, response) => {
+  return response.json(projects)
+})
+
+app.post('/projects', (request, response) => {
+  return response.json(projects)
+})
+
+app.put('/projects/:id', (request, response) => {
+  return response.json(projects)
+})
+
+app.delete('/projects/:id', (request, response) => {
+  return response.json(projects)
 })
 
 app.listen(PORT, () => {
