86ac5e330ae741bf17a46e9498db5a51cce49915
Fabiano Santos Ramos <fabianosantosramos1978@gmail.com>
2025-03-30 17:05:28 -0300

    crud completed

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 require('dotenv').config()
+const { v4: uuid } = require('uuid')
 const express = require('express')
 const app = express()
+app.use(express.json())
 
 const PORT = process.env.PORT
 
-const projects = [
-  "Project 1",
-  "Project 2"
-]
+const projects = []
 
 app.get('/', (request, response) => {
   return response.json({ message: "Welcome to the API" })
@@ -18,15 +17,58 @@ app.get('/projects', (request, response) => {
 })
 
 app.post('/projects', (request, response) => {
-  return response.json(projects)
+  const { name, owner } = request.body
+  const project = {
+    id: uuid(),
+    name,
+    owner
+  }
+  projects.push(project)
+  return response.status(201).json(projects)
 })
 
 app.put('/projects/:id', (request, response) => {
-  return response.json(projects)
+  const { id } = request.params
+  const { name, owner } = request.body
+
+  const projectIndex = projects.findIndex(project => project.id === id)
+
+  if (projectIndex < 0) {
+    return response.status(404).json({
+      Error: `Project with id ${id} not found`
+    })
+  }
+
+  if (!name || !owner) {
+    return response.status(400).json({
+      error: 'Name and Owner are required...'
+    })
+  }
+
+  const project = {
+    id, name, owner
+  }
+
+  projects[projectIndex] = project
+
+
+  return response.json(project)
 })
 
 app.delete('/projects/:id', (request, response) => {
-  return response.json(projects)
+  const { id } = request.params
+
+  const projectIndex = projects.findIndex(project => project.id === id)
+
+  if (projectIndex < 0) {
+    return response.status(404).json({
+      Error: `Project with id ${id} not found`
+    })
+  }
+
+  projects.splice(projectIndex, 1)
+
+  return response.status(204).send()
 })
 
 app.listen(PORT, () => {
